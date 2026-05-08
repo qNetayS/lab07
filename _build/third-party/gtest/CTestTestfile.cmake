5ed6f8df3c7d8c41899c15ddf92935b5f6a5e9c7
qNetayS <petrenk007@yandex.ru>
2026-05-08 23:55:34 +0300

    Remove _build and add .gitignore

--- _build/third-party/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vboxuser/qNetayS/lab07/third-party/gtest
-# Build directory: /home/vboxuser/qNetayS/lab07/_build/third-party/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
